fe24efdc631a95fbb32e74f0f18f52255dab80c6
dpro <dbintou008@gmail.com>
2023-04-13 12:14:50 +0200

    first commit

---
+++ src/views/TodosIssues.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <h1>todoリスト</h1>
+    <!-- todo入力フォーム -->
+    <form @submit.prevent="addTodo()">
+      <el-input placeholder="todo" v-model="todo"></el-input>
+    </form>
+    <el-row :gutter="12">
+      <!-- todo表示エリア -->
+      <el-col :span="12" v-for="( todo, index ) in todos" :key="index">
+        <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
+          <el-row :gutter="12">
+            <el-col :span="21">{{ todo }}</el-col>
+            <el-col :span="3">
+              <el-button @click="removeTodo(index)" type="success" icon="el-icon-check" circle></el-button>
+            </el-col>
+          </el-row>
+        </el-card>
+      </el-col>
+      <!-- issue表示エリア -->
+      <el-col :span="12" v-for="( issue, index ) in issues" :key="issue.id">
+        <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
+          <el-row :gutter="12">
+            <el-col :span="21">{{ issue.title }}</el-col>
+            <el-col :span="3">
+              <el-button @click="closeIssue(index)" type="success" icon="el-icon-check" circle></el-button>
+            </el-col>
+          </el-row>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: `${process.env.VUE_APP_GITHUB_ENDPOINT}`,
+  headers: {
+    'Authorization': `token ${process.env.VUE_APP_GITHUB_TOKEN}`,
+    'Accept': 'application/vnd.github.v3+json',
+    'Content-Type':'application/json',
+  },
+})
+
+export default {
+  name: 'TodosIssues',
+  data () {
+    return {
+      todo: '',
+      todos: [],
+      issues: []
+    }
+  },
+  methods: {
+    // ここからtodoの管理
+    addTodo(){
+      this.todos.push(this.todo);
+      this.todo= '';
+    },
+    removeTodo(index){
+      this.todos.splice(index, 1);
+    },
+    // ここからissueの管理
+    closeIssue(index){
+      const target = this.issues[index];
+      client.patch(`/issues/${target.number}`,
+          {
+            state: "closed"
+          },
+        )
+        .then(() => {
+         this.issues.splice(index, 1);
+      })
+    },
+    getIssues() {
+      client.get('issues')
+        .then((res) => {
+          this.issues = res.data
+      })
+    }
+  },
+  created() {
+    this.getIssues();
+  }
+}
+</script>
