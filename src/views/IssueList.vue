da291b26c53f485c876d1d6c5cda6160d2f427d7
dpro <dbintou008@gmail.com>
2023-04-13 14:39:43 +0200

    first commit

--- src/views/IssueList.vue
+++ src/views/IssueList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>issueリスト</h1>
+    <h1>issueList</h1>
     <el-button type="success" @click="getIssues()">issue取得</el-button>
     <el-row :gutter="12">
       <el-col :span="12" v-for="issue in issues" :key="issue.id">
@@ -22,43 +22,40 @@
 import axios from 'axios';
 
 const client = axios.create({
-  // 環境変数に変更
-  baseURL: `${process.env.VUE_APP_GITHUB_ENDPOINT}`, 
-  headers: {
-    'Accept': 'application/vnd.github.v3+json',
-    'Content-Type':'application/json',
-    // 環境変数に変更
-    'Authorization': `token ${process.env.VUE_APP_GITHUB_TOKEN}`
-  },
+baseURL: `${process.env.VUE_APP_GITHUB_ENDPOINT}`, 
+headers: {
+  'Accept': 'application/vnd.github.v3+json',
+  'Content-Type':'application/json',
+  'Authorization': `token ${process.env.VUE_APP_GITHUB_TOKEN}`
+},
 })
-
 export default {
-  name: ' IssueList',
-  data() {
-    return {
-      issues: []
-    }
-  },
-  methods: {
-    getIssues() {
-      client.get('/issues')
-        .then((res) => {
-          this.issues = res.data;
-      })
-    },
-    // 新しく作成したmethod
-    closeIssue(number){
-      client.patch(`/issues/${number}`, // --コード2
-          {
-            state: 'closed'
-          },
-        )
-        .then(() => {
-          this.issues.some((v, i) => { // --コード3
-            if(v.number == number) this.issues.splice(i, 1); // --コード4
-          })
-      })
-    },
+name: ' IssueList',
+data() {
+  return {
+    issues: []
   }
+},
+methods: {
+  getIssues() {
+    client.get('/issues')
+      .then((res) => {
+        this.issues = res.data;
+    })
+  },
+  // 新しく作成したmethod
+  closeIssue(number){
+    client.patch(`/issues/${number}`, // --コード2
+        {
+          state: 'closed'
+        },
+      )
+      .then(() => {
+        this.issues.some((v, i) => { // --コード3
+          if(v.number == number) this.issues.splice(i, 1); // --コード4
+        })
+    })
+  },
+}
 }
 </script>
--- src/views/TodosIssues.vue
+++ src/views/TodosIssues.vue
@@ -7,18 +7,9 @@
     </form>
     <el-row :gutter="12">
       <!-- todo表示エリア -->
-      <el-col :span="12" v-for="( todo, index ) in todos" :key="index">
-        <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
-          <el-row :gutter="12">
-            <el-col :span="21">{{ todo }}</el-col>
-            <el-col :span="3">
-              <el-button @click="removeTodo(index)" type="success" icon="el-icon-check" circle></el-button>
-            </el-col>
-          </el-row>
-        </el-card>
-      </el-col>
+      <TodoItem v-for="( todo, index ) in todos" :key="index" />
       <!-- issue表示エリア -->
-      <el-col :span="12" v-for="( issue, index ) in issues" :key="issue.id">
+      <el-col :span="12"  v-for="( issue, index ) in issues" :key="issue.id">
         <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
           <el-row :gutter="12">
             <el-col :span="21">{{ issue.title }}</el-col>
@@ -34,6 +25,7 @@
 
 <script>
 import axios from 'axios';
+import TodoItem from '@/components/TodoItem';
 
 const client = axios.create({
   baseURL: `${process.env.VUE_APP_GITHUB_ENDPOINT}`,
@@ -46,6 +38,9 @@ const client = axios.create({
 
 export default {
   name: 'TodosIssues',
+  components: {
+    TodoItem
+  },
   data () {
     return {
       todo: '',
@@ -54,7 +49,6 @@ export default {
     }
   },
   methods: {
-    // ここからtodoの管理
     addTodo(){
       this.todos.push(this.todo);
       this.todo= '';
@@ -62,12 +56,11 @@ export default {
     removeTodo(index){
       this.todos.splice(index, 1);
     },
-    // ここからissueの管理
     closeIssue(index){
       const target = this.issues[index];
       client.patch(`/issues/${target.number}`,
           {
-            state: "closed"
+            state: 'closed'
           },
         )
         .then(() => {
